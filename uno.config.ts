0aa926182d5e13fd767eb2ab77c49e6cd7c98a90
amaury-tobias <amaury.tobiasqr@gmail.com>
2023-09-08 11:58:51 -0600

    fix: vertical text direction on nuocss

--- uno.config.ts
+++ uno.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
         .scrollbar-hide::-webkit-scrollbar {display: none;}`
       },
     ],
-    ['vertical-lr', { 'writing-mode': 'vertical-lr' }],
+    ['vertical-rl', { 'writing-mode': 'vertical-rl' }],
   ],
   theme: {
     fontFamily: {
