82de52e29c60cb032f52b2df380b177a1288cce0
amaury-tobias <amaury.tobiasqr@gmail.com>
2023-09-08 11:59:16 -0600

    feat: add poker card component

---
+++ components/Poker/Card.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    peer
+    class="flex bg-white rounded-xl border shadow-lg overflow-hidden transition-all-400 ease-in-out relative left-0 [&:not(:first-child)]:-ml-12 hover:-translate-y-5 peer-hover:left-12"
+  >
+    <slot />
+  </div>
+</template>
