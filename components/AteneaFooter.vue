09089692caddfe241ff616f460b8fa00cd23ff3b
amaury-tobias <amaury.tobiasqr@gmail.com>
2023-09-12 14:24:49 -0600

    feat: add footer

---
+++ components/AteneaFooter.vue
@@ -0,0 +1,116 @@
+<template>
+  <footer>
+    <div class="relative z-1 h-24">
+      <div class="absolute -bottom-6 w-full">
+        <div
+          class="h-full w-11/12 mx-auto bg-[rgb(177_191_146)] rounded-xl p-4 text-center"
+        >
+          <p class="text-neutral-100 font-semibold text-lg">
+            !Supera tus limites!
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="px-12 pb-12 pt-16 grid gap-4 md:grid-cols-4 bg-neutral-700 text-neutral-200"
+    >
+      <div class="grid gap-1 content-center md:justify-center">
+        <NuxtLink
+          to="/"
+          class="flex items-center gap-1"
+        >
+          <img
+            src="@/assets/atenea_logo.png"
+            class="h-10 w-10 drop-shadow-sm"
+          />
+          <span class="font-semibold text-[rgb(177_191_146)] drop-shadow-sm">
+            ATENEA
+          </span>
+        </NuxtLink>
+
+        <p class="text-xs text-neutral-300 font-extralight">
+          ¡Supera tus limites!
+        </p>
+        <p class="text-xs text-neutral-300 font-extralight">Copyright © 2023</p>
+      </div>
+
+      <div class="grid gap-2 md:grid-cols-3 md:col-span-3">
+        <div
+          class="grid md:row-span-2 gap-2 content-center text-sm font-extralight"
+        >
+          <NuxtLink
+            to="/nutricion"
+            class="border-l-3 border-[rgb(177_191_146)] pl-2"
+          >
+            Nutrición
+          </NuxtLink>
+
+          <NuxtLink
+            to="/epigenetica"
+            class="border-l-3 border-[rgb(177_191_146)] pl-2"
+          >
+            Epigenética
+          </NuxtLink>
+        </div>
+
+        <div
+          class="grid md:row-span-2 gap-2 content-center text-sm font-extralight"
+        >
+          <NuxtLink
+            to="/nosotros"
+            class="border-l-3 border-[rgb(177_191_146)] pl-2"
+          >
+            Nosotros
+          </NuxtLink>
+        </div>
+
+        <div
+          class="grid justify-start md:justify-center content-center text-sm"
+        >
+          <a
+            href="https://api.whatsapp.com/send?phone=5214443159190&text=Hola%2C%20me%20gustaría%20agendar%20una%20cita%20nutricional."
+            target="_blank"
+            rel="noopener noreferrer"
+            role="button"
+            class="rounded-xl bg-[rgb(177_191_146)] text-neutral-600 font-semibold py-2 px-6"
+          >
+            <div class="flex gap-1 justify-center items-center">
+              Contacto
+              <UnoIcon class="i-ph:whatsapp-logo w-4 h-4" />
+            </div>
+          </a>
+        </div>
+
+        <div class="flex gap-4 justify-start md:justify-center py-4">
+          <a
+            href="https://www.instagram.com/ateneanutricion/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <UnoIcon class="i-ph:instagram-logo w-6 h-6" />
+          </a>
+          <a
+            href="https://www.facebook.com/YetlanetziPortillo/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <UnoIcon class="i-ph:facebook-logo w-6 h-6" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/mitzivillafaña"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <UnoIcon class="i-ph:linkedin-logo w-6 h-6" />
+          </a>
+          <a
+            href="mailto:contacto@ateneanutricion.com?Subject=Cita%20Nutricional"
+          >
+            <UnoIcon class="i-ph:envelope-simple w-6 h-6" />
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,55 +2,10 @@
   <div>
     <AteneaNavBar />
 
-    <main class="container max-w-[80ch] mx-auto font-albert-sans">
+    <main class="container lg:max-w-[80ch] mx-auto font-albert-sans">
       <slot />
     </main>
 
-    <footer class="sm:h-xs grid grid-rows-2">
-      <div class="relative z-1">
-        <div class="absolute -bottom-6 w-full">
-          <div
-            class="h-full w-11/12 mx-auto bg-[#15CDCA] rounded-xl p-4 text-center"
-          >
-            <p class="text-neutral-200 font-semibold text-lg">ATENEA</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="pt-6 bg-neutral-700 text-neutral-200 grid">
-        <div class="p-4 space-y-4">
-          <NuxtLink
-            to="/"
-            class="flex items-center gap-1"
-          >
-            <img
-              src="@/assets/atenea_logo.png"
-              class="h-10 w-10 drop-shadow-sm"
-            />
-            <span class="font-semibold text-[rgb(177_191_146)] drop-shadow-sm">
-              ATENEA
-            </span>
-          </NuxtLink>
-
-          <p>
-            Mi nombre es Mitzi y quiero ayudarte a superar tus metas, nuestro
-            objetivo principal será optimizar tu salud, utilizando la mejor arma
-            que es la Nutrición de excelencia y calidad
-          </p>
-        </div>
-
-        <div class="p-4">
-          <p>Nosotros</p>
-          <p>Contactanos <UnoIcon class="i-ph:whatsapp-logo-bold w-6 h-6" /></p>
-        </div>
-
-        <div class="p-4 flex">
-          <UnoIcon class="i-ph:instagram-logo-bold w-6 h-6" />
-          <UnoIcon class="i-ph:envelope-simple-bold w-6 h-6" />
-          <UnoIcon class="i-ph:facebook-logo-bold w-6 h-6" />
-          <UnoIcon class="i-ph:linkedin-logo-bold w-6 h-6" />
-        </div>
-      </div>
-    </footer>
+    <AteneaFooter />
   </div>
 </template>
