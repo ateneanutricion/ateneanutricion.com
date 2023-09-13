ab0c0d9a1282eb1273ca94ae5ce6541a3dfc3042
amaury-tobias <amaury.tobiasqr@gmail.com>
2023-09-13 17:54:31 -0600

    feat(page):  nutricion

--- pages/nutricion.vue
+++ pages/nutricion.vue
@@ -12,14 +12,11 @@
 
       <article class="md:order-first grid content-center gap-4">
         <div class="font-semibold">
-          <span class="text-5xl">
+          <span class="text-4xl">
             ¿Quieres llevar tu
-            <span class="text-[#4F80E2]">desempeño</span>
-          </span>
-          <br />
-          <span class="text-5xl">
+            <span class="text-5xl text-[#4F80E2]">desempeño</span>
             al siguiente
-            <span class="text-[#4F80E2]">nivel?</span>
+            <span class="text-5xl text-[#4F80E2]">nivel?</span>
           </span>
         </div>
 
@@ -43,177 +40,161 @@
       </article>
     </header>
 
-    <div class="col-span-2 p-4 rounded-3xl grid-rows-2">
-      <div class="grid auto-rows-[192px] grid-cols-4 gap-4">
-        <!-- 1 -->
-        <div class="row-span-2 col-span-2 rounded-xl bg-[rgb(75_167_139)] p-4">
-          <div class="h-full flex flex-col justify-between">
-            <h3 class="font-black text-6xl">Estudios</h3>
+    <section class="grid gap-4 md:grid-cols-3 auto-rows-48">
+      <article
+        group
+        class="relative rounded-xl overflow-hidden"
+      >
+        <NuxtImg
+          src="/nutricion/fitness.jpeg"
+          loading="lazy"
+          preset="hero"
+          fit="cover"
+          class="object-cover transition-300 group-hover:scale-110"
+        />
 
-            <div class="flex gap-4 items-end justify-start tracking-tighter">
-              <h3
-                class="font-black text-5xl"
-                style="writing-mode: vertical-lr"
-              >
-                epigenéticos
-              </h3>
-              <p class="font-medium text-base">
-                Mi nombre es Mitzi y quiero ayudarte a superar tus metas,
-                nuestro objetivo principal será optimizar tu salud, utilizando
-                la mejor arma que es la Nutrición de excelencia y calidad
-              </p>
-            </div>
+        <div class="absolute h-full w-full top-0 grid content-center">
+          <div
+            class="px-4 py-2 w-full text-center text-3xl md:text-lg text-neutral-200 bg-white/30 backdrop-blur"
+          >
+            <p class="font-medium tracking-tighter">
+              Composición corporal & fitness
+            </p>
           </div>
         </div>
+      </article>
 
-        <!-- Nutricion -->
-        <div
-          group
-          class="row-span-1 col-span-1 rounded-xl bg-[rgb(169_173_217)] p-4"
-        >
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-mdi:fruit-cherries h-6 w-6 group-hover:animate-wobble"
-            />
+      <article
+        group
+        class="relative rounded-xl overflow-hidden"
+      >
+        <NuxtImg
+          src="/nutricion/rendimiento.jpeg"
+          loading="lazy"
+          preset="hero"
+          fit="cover"
+          class="object-cover transition-300 group-hover:scale-110"
+        />
 
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">Vitaminas</h3>
-              <p class="text-xs">Enfocada en tus objetivos</p>
-            </div>
-
-            <div class="flex justify-between">
-              <AteneaTag class="bg-black text-white border-0!">Pick</AteneaTag>
-              <AteneaTag>Pick</AteneaTag>
-            </div>
+        <div class="absolute h-full w-full top-0 grid content-center">
+          <div
+            class="px-4 py-2 w-full text-center text-3xl md:text-lg text-neutral-200 bg-white/30 backdrop-blur"
+          >
+            <p class="font-medium tracking-tighter">
+              Alto rendimiento & deporte
+            </p>
           </div>
         </div>
+      </article>
 
-        <div class="row-span-1 col-span-1 rounded-xl bg-[rgb(200_233_94)] p-4">
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-mdi:head-snowflake-outline h-6 w-6 group-hover:animate-wobble"
-            />
+      <article
+        group
+        class="relative rounded-xl overflow-hidden"
+      >
+        <NuxtImg
+          src="/nutricion/clinica.jpg"
+          loading="lazy"
+          preset="hero"
+          fit="cover"
+          class="object-cover transition-300 group-hover:scale-110"
+        />
 
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">
-                Antioxidantes
-              </h3>
-              <p class="text-xs">
-                Coaching psiconutricional para trabajar tu motivación y metas
-              </p>
-            </div>
-
-            <AteneaTag class="bg-black text-white border-0!">Pick</AteneaTag>
+        <div class="absolute h-full w-full top-0 grid content-center">
+          <div
+            class="px-4 py-2 w-full text-center text-3xl md:text-lg text-neutral-200 bg-white/30 backdrop-blur"
+          >
+            <p class="font-medium tracking-tighter">Clínica & metabólica</p>
           </div>
         </div>
+      </article>
+    </section>
 
-        <div
-          group
-          class="row-span-1 col-span-1 rounded-xl bg-[rgb(169_173_217)] p-4"
-        >
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-mdi:fruit-cherries h-6 w-6 group-hover:animate-wobble"
-            />
+    <section class="space-y-8 bg-neutral-100 p-4">
+      <article class="grid md:grid-cols-2">
+        <div class="font-semibold h-full grid content-center p-4">
+          <span class="text-4xl">
+            Composición
+            <span class="text-[#4F80E2]">corporal</span>
+            &
+            <span class="text-[#4F80E2]">fitness</span>
+          </span>
+        </div>
+        <div class="p-4 md:pl-0 grid content-center">
+          <p>
+            Sabemos que cada cuerpo es diferente y nos apasiona personalizar
+            cada plan con un enfoque personalizado. Así como perder grasa
+            corporal, ganar masa muscular, mejorar su salud u optimizar su
+            rendimiento, creamos un plan de nutrición de acuerdo con sus
+            objetivos.
+          </p>
+        </div>
+      </article>
 
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">Minerales</h3>
-              <p class="text-xs">Enfocada en tus objetivos</p>
-            </div>
+      <article class="grid md:grid-cols-2">
+        <div class="font-semibold h-full grid content-center p-4 md:order-last">
+          <span class="text-4xl">
+            Alto
+            <span class="text-[#4F80E2]">rendimiento</span>
+            &
+            <span class="text-[#4F80E2]">deporte</span>
+          </span>
+        </div>
+        <div class="p-4 md:pl-0 grid content-center">
+          <div class="space-y-4">
+            <p>
+              Entendemos la importancia de la nutrición en el rendimiento
+              deportivo, cada atleta y deporte es único y requiere un programa
+              personalizado adaptado sus necesidades.
+            </p>
+            <p>
+              Desde deportistas amateur hasta atletas profesionales, creamos un
+              programa y periodización nutricional para alcanzar tus metas de
+              composición corporal y rendimiento deportivo.
+            </p>
 
-            <div class="flex justify-between">
-              <AteneaTag class="bg-black text-white border-0!">Pick</AteneaTag>
-              <AteneaTag>Pick</AteneaTag>
-            </div>
+            <p class="text-[#4F80E2] font-semibold text-lg">
+              Haz de la nutrición tu ventaja competitiva.
+            </p>
+
+            <p>
+              En general, el objetivo de un nutricionista atleta es ayudar a los
+              atletas a maximizar su potencial a través de una nutrición
+              adecuada y ayudarlos a alcanzar sus objetivos de rendimiento de
+              una manera segura y saludable.
+            </p>
           </div>
         </div>
+      </article>
 
-        <!-- CONTACTO -->
-        <div
-          class="row-span-2 col-span-1 rounded-xl bg-[rgb(33_50_216)] overflow-hidden"
-        >
-          <div class="relative h-full flex flex-col justify-end">
-            <img
-              src="@/assets/workout.jpg"
-              class="h-full w-full object-cover transition transition-duration-300 hover:scale-110"
-            />
+      <article class="grid md:grid-cols-2">
+        <div class="font-semibold h-full grid content-center p-4">
+          <span class="text-4xl">
+            <span class="text-[#4F80E2]">Clínica</span>
+            &
+            <span class="text-[#4F80E2]">metabólica</span>
+          </span>
+        </div>
+        <div class="p-4 md:pl-0 grid content-center">
+          <div class="space-y-4">
+            <p>
+              Eres único. Tu Nutrición también debería serlo. Áreas
+              especializadas de nutrición y manejo de enfermedades en las que se
+              enfocan nuestros dietistas.
+            </p>
 
-            <div class="absolute bottom-2 left-2 text-white tracking-tighter">
-              <h3 class="font-black text-3xl">CONTACTO</h3>
-              <p class="text-xs">+52 444 123 456</p>
-            </div>
+            <ul class="list-disc list-inside">
+              <li>Diabetes</li>
+              <li>SOAP</li>
+              <li>Obesidad & control de peso</li>
+              <li>Problemas gastrointestinales</li>
+              <li>Embarazo & lactancia</li>
+              <li>Trastornos conducta alimentaria</li>
+              <li>Problemas cardiovasculares</li>
+              <li>Recuperación cirugías bariátricas</li>
+            </ul>
           </div>
         </div>
-
-        <!-- EPIGENÉTICA -->
-        <div
-          group
-          class="row-span-1 col-span-1 rounded-xl bg-[rgb(251_252_211)] p-4"
-        >
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-ph:dna-duotone h-6 w-6 group-hover:animate-wobble"
-            />
-
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">
-                Interferencias
-              </h3>
-              <p class="text-xs">
-                Coaching psiconutricional para trabajar tu motivación
-              </p>
-            </div>
-          </div>
-        </div>
-        <!-- ENTRENAMIENTO -->
-        <div
-          group
-          class="row-span-1 col-span-1 rounded-xl border-2 border-slate-400/10 bg-[rgb(252_235_211)] p-4"
-        >
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-mdi:weight-lifter h-6 w-6 group-hover:animate-wobble"
-            />
-
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">
-                Ácidos Grasos
-              </h3>
-              <p class="text-xs">Programa de entrenamiento fisico</p>
-            </div>
-
-            <div class="flex justify-between">
-              <AteneaTag class="bg-black text-white border-0!">Pick</AteneaTag>
-              <AteneaTag>Pick</AteneaTag>
-            </div>
-          </div>
-        </div>
-        <!-- PSICONUTRICIÓN -->
-        <div
-          group
-          class="row-span-1 col-span-1 rounded-xl border-2 border-slate-400/10 bg-[rgb(247_211_207)] p-4"
-        >
-          <div class="h-full flex flex-col justify-between">
-            <UnoIcon
-              class="i-mdi:head-snowflake-outline h-6 w-6 group-hover:animate-wobble"
-            />
-
-            <div>
-              <h3 class="font-extrabold text-xl tracking-tighter">
-                Aminoácidos
-              </h3>
-              <p class="text-xs">
-                Coaching psiconutricional para trabajar tu motivación y metas
-              </p>
-            </div>
-
-            <div class="flex justify-between">
-              <AteneaTag class="bg-black text-white border-0!">Pick</AteneaTag>
-              <AteneaTag>Pick</AteneaTag>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      </article>
+    </section>
   </div>
 </template>
