0ee5b0e1c98e870df9b4471a92ac64e775216d83
amaury-tobias <amaury.tobiasqr@gmail.com>
2024-01-18 22:49:14 -0600

    chore: fix eslinnt config

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,18 @@
 import antfu from '@antfu/eslint-config'
 import unocss from '@unocss/eslint-config/flat'
 
-export default [
-  ...antfu({
-    overrides: {
-      vue: {
-        'vue/max-attributes-per-line': ['error', {
-          singleline: {
-            max: 1,
-          },
-          multiline: {
-            max: 1,
-          },
-        }],
-      },
+export default antfu({
+  unocss: true,
+  overrides: {
+    vue: {
+      'vue/max-attributes-per-line': ['error', {
+        singleline: {
+          max: 1,
+        },
+        multiline: {
+          max: 1,
+        },
+      }],
     },
-  }),
-  unocss,
-
-]
+  },
+})
